c60b402db9cf40e5655fbf1db774f90c0645c92e
Floriano Delishaj <floriano.delishaj@yolo-group.com>
2026-02-24 02:47:28 +0100

    rimozione di una voce

--- storefront/src/modules/products/components/product-purchase-sidebar/shipping-info-cards.tsx
+++ storefront/src/modules/products/components/product-purchase-sidebar/shipping-info-cards.tsx
@@ -22,18 +22,6 @@ const ShippingInfoCards = () => {
 
           <div className="flex items-start gap-2 p-3 bg-white border border-black rounded-md">
             <div className="shrink-0 w-6 h-6 flex items-center justify-center bg-green-400 border border-black rounded-md">
-              <Refresh className="w-4 h-4" />
-            </div>
-            <div>
-              <span className="font-bold text-black block mb-0.5 text-xs">
-                Cambi Semplici
-              </span>
-              <p className="text-gray-700 text-xs">Cambio taglia gratuito</p>
-            </div>
-          </div>
-
-          <div className="flex items-start gap-2 p-3 bg-white border border-black rounded-md">
-            <div className="shrink-0 w-6 h-6 flex items-center justify-center bg-green-400 border border-black rounded-md">
               <Back className="w-4 h-4" />
             </div>
             <div>
